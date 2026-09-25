fae4cd417a994a79f1db61da00ef265e828185d8
endtheline <139571653+endtheline@users.noreply.github.com>
2026-09-25 09:32:36 -0400

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,23 @@
+import requests
+
+URL = "https://gacetaoficialdebolivia.gob.bo/normas/listadonor/10"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+try:
+    respuesta = requests.get(
+        URL,
+        headers=headers,
+        timeout=30
+    )
+
+    print("Código HTTP:", respuesta.status_code)
+    print("URL final:", respuesta.url)
+    print("Primeros 500 caracteres:")
+    print(respuesta.text[:500])
+
+except Exception as e:
+    print("Error de conexión:")
+    print(e)
